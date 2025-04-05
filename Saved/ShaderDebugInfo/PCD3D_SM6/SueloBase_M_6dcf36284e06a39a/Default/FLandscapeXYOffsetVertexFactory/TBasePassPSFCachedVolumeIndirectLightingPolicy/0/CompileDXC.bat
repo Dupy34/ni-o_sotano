3b2251e582aad7d400ede61abd6c3f0a5d6b4847
Dupy34 <133169378+Dupy34@users.noreply.github.com>
2025-04-05 04:16:42 -0600

    implemenracion de todo

--- Saved/ShaderDebugInfo/PCD3D_SM6/SueloBase_M_6dcf36284e06a39a/Default/FLandscapeXYOffsetVertexFactory/TBasePassPSFCachedVolumeIndirectLightingPolicy/0/CompileDXC.bat
+++ Saved/ShaderDebugInfo/PCD3D_SM6/SueloBase_M_6dcf36284e06a39a/Default/FLandscapeXYOffsetVertexFactory/TBasePassPSFCachedVolumeIndirectLightingPolicy/0/CompileDXC.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-SET DXC="C:\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+SET DXC="C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
 IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	ECHO Couldn't find dxc.exe under "C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
 	GOTO :END
 )
 %DXC% -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T ps_6_6 -E MainPS -Fc BasePassPixelShader.d3dasm -Fo BasePassPixelShader.dxil BasePassPixelShader
